82c75a10017f515801331fe0dd41621c39de7702
cchrisking <christou2c@gmail.com>
2025-11-17 22:32:00 +0100

    added a folder

---
+++ programmation_structuree/src/strings/string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  char unmessage[]="premier message";
+  char *pmessage;
+  pmessage = "premier message";
+  printf("%s\n", unmessage);
+  printf("%s\n", pmessage);
+  return 0;
+}
